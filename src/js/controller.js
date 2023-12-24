d80947b76a06bb257ba457f7ebd24dbfbde0e778
mersey01 <mersey01@sky.com>
2023-12-24 16:32:40 +0000

    Added welcome message feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -118,6 +118,7 @@ const controlStateFromStorage = function () {
 const newFeature = function() {
   alert('Welcome to my app with new feature ');
 }
+
 const init = () => {
   recipeView.addHandlerRender(controlRecipe);
   recipeView.addHandlerUpdateServings(controlServings);
